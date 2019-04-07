e6f9a2d398d5ed5c5cd22eb21daef7a2230d10a4
Zach Tibbitts <zachtib@gmail.com>
2019-04-07 14:05:46 -0400

    layout for SearchView

--- app/src/main/java/com/zachtib/mtgcodex/ui/search/SearchView.kt
+++ app/src/main/java/com/zachtib/mtgcodex/ui/search/SearchView.kt
@@ -2,9 +2,10 @@ package com.zachtib.mtgcodex.ui.search
 
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
+import com.zachtib.mtgcodex.R
 import com.zachtib.mtgcodex.framework.BaseFragment
 
-class SearchView : BaseFragment() {
+class SearchView : BaseFragment(R.layout.search_fragment) {
     private val viewModel: SearchViewModel by fragmentViewModel()
 
     override fun invalidate() = withState(viewModel) { state ->
